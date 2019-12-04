da7c3657a78e10253f871f48641af436ee169dc6
Squizzy121 <danil-b@ukr.net>
2019-12-04 18:15:08 +0200

    Второклассная задача

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            int n = 0;
+            int l = x.Length; 
+            for (int q = 0;q <= l-1; q++)
+            {
+                if (x[q]=='6'||x[q]=='9'||x[q]=='0')
+                {
+                    ++n;
+                }
+                if (x[q]=='8')
+                {
+                    n += 2;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
